9f92e38a95d8656e2b54279ee101b158301dc0b8
jude <jude@affle.com>
2015-01-17 18:24:44 +0800

    intital commit

---
+++ string-working-with-text/src/Application.java
@@ -0,0 +1,27 @@
+
+public class Application
+    {
+    public static void main(String[] args)
+        {
+        int myInt = 7;
+
+        String text = "Hello";
+
+        String blank = " ";
+
+        String name = "Bob";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "Bob");
+
+        System.out.println("My Interger is :" + myInt);
+
+        double myDouble = 7.8;
+
+        System.out.println("my Number is : " + myDouble + ".");
+
+        }
+    }
---
+++ strings-working-with-text/src/strings/working/with/text/StringsWorkingWithText.java
@@ -0,0 +1,8 @@
+public class StringsWorkingWithText
+    {
+
+    public static void main(String[] args)
+        {
+        }
+
+    }
